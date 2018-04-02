f8e408911b630866f7159eb0114295a0df0124da
David Scherer-ODell <dscherer21@gmail.com>
2018-04-02 17:51:18 -0500

    created landing page, added background image, and header

---
+++ src/components/titlePage/titlePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './titlePage.css';
+
+const Home = () =>
+  <div>
+    <div id='title' className='text-center'>
+      <h1><span id='rainbow'>PONG 2180</span></h1>
+      <h2>Click Here to Start!</h2>
+    </div>
+  </div>;
+
+export default Home;
