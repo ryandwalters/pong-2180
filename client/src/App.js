6856f8e478ebabe60cfb5bbb5fe1249bdd047d5e
ryandwalters <ryandwalters@gmail.com>
2018-04-11 15:44:59 -0500

    creating components

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,95 @@ import './App.css';
 import Home from './components/titlePage/';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Home/>
+        <Home />
+
+        <header>header</header>
+
+        <main className="container">
+
+          <div className="row">
+            {/* Ball Arrival Time and any other stats we want */}
+            <div className="col-xs-3" >
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Arrival Time</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+
+            </div>
+
+            {/* Map display */}
+
+            <div className="col-xs-6" >
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Map</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+            </div>
+
+
+            {/* scoreboar and possible twitter and text buttons */}
+            <div className="col-xs-3" >
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Scoreboard</h3>
+                </div>
+                <div class="panel-body">
+                  Panel content
+                </div>
+              </div>
+            </div>
+            <div className="col-xs-12 b" >
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title">HIT THE BALL!</h3>
+                </div>
+                <div class="panel-body">
+                  <button>hit the ball!</button>
+                </div>
+              </div>
+
+
+              </div>
+
+
+            
+
+          </div>
+
+
+
+        </main>
+
+
+
+
+        <footer>footer</footer>
+
+
       </div>
-    );
-  }
+
+
+
+
+
+
+
+
+
+      
+    )};
 }
 
 export default App;
