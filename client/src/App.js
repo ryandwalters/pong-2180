4cab9b5ae8340ee1494e9efd55ff32ac7dcdb19f
David Scherer-ODell <dscherer21@gmail.com>
2018-04-11 20:39:04 -0500

    made some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/titlePage/';
+import TitlePage from './components/titlePage/';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+
+/*document.querySelectorAll('#loadHome').click(function () {
+  //change  style to display: none; to hide it
+  document.querySelectorAll('#body').attr('style', 'display: initial;');
+  //change main3 style to display: initial: to show it
+  document.querySelectorAll('#title').attr('style', 'display: none;');
+});*/
+
+function handleClick(event) {
+  //change  style to display: none; to hide it
+  document.querySelectorAll('#body').attr('style', 'display: initial;');
+  //change main3 style to display: initial: to show it
+  document.querySelectorAll('#title').attr('style', 'display: none;');
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home/>
+        <TitlePage/>
+        <div id='body'>
+          <header>header</header>
+          <main>main</main>
+          <footer>footer</footer>
+        </div>
       </div>
     );
   }
--- client/src/components/titlePage/titlePage.js
+++ client/src/components/titlePage/titlePage.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './titlePage.css';
 
-const Home = () =>
-  <div>
-    <div id='title' className='text-center'>
+const TitlePage = () =>
+  <div id='title'>
+    <div className='text-center'>
       <h1><span id='rainbow'>PONG 2180</span></h1>
       <hr/>
-      <Link href='#'><h2>Click Here to Start!</h2></Link>
+      <a onClick="return handleClick(event);"><h2>Click Here to Start!</h2></a>
     </div>
   </div>;
 
-export default Home;
+export default TitlePage;
