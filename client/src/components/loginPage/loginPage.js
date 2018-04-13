24f3e47dbfb927a6e8e21157df298bfd7297cde4
David Scherer-ODell <dscherer21@gmail.com>
2018-04-13 15:16:16 -0500

    created loginPage

---
+++ client/src/components/loginPage/loginPage.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import './loginPage.css';
+
+
+const LoginPage = () => {
+    return (
+        <div className='row'>
+            <form className="navbar-form navbar-left" role="search">
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Search"/>
+                </div>
+                    <button type="submit" className="btn btn-default">Submit</button>
+            </form>
+
+                <form className="navbar-form navbar-left" role="search">
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Search"/>
+                    </div>
+                        <button type="submit" className="btn btn-default">Submit</button>
+                </form>
+        </div>
+                )
+            }
+            
+export default LoginPage;
--- client/src/components/routes/routes.js
+++ client/src/components/routes/routes.js
@@ -1,5 +1,6 @@
 import TitlePage from './../titlePage/';
 import Main1 from './../Main1/';
+import LoginPage from './../loginPage/';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import './routes.css';
@@ -7,11 +8,13 @@ import './routes.css';
 
 const Routes = () => {
     return(
-        <main className='container'>
-            Hello
-            {/*<TitlePage/>*/}
-            <Main1/>
-        </main>
+        <Router>
+            <main className='container'>
+                {/*<TitlePage/>*/}
+                {/*<Main1/>*/}
+                <LoginPage/>
+            </main>
+        </Router>
     )
 }
 
