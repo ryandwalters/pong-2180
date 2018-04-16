ebb65e3e2c54cb287206748bbb736e3d9d52757b
David Scherer-ODell <dscherer21@gmail.com>
2018-04-16 18:24:41 -0500

    style changes

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <header className='container'>
                 <div className="row text-center">
                     <div className='col-xs-8'>
-                        <h2 className='bungeeFont'>PONG 2180</h2>
+                        <Link to='/'><h2 className='bungeeFont'>PONG 2180</h2></Link>
                     </div>
                     <div className='col-xs-3'>              
                         <Link to="/gamestart">
