50ed191ff21639af8ee563c5f259f89182528ca8
David Scherer-ODell <dscherer21@gmail.com>
2018-04-16 16:53:23 -0500

    did some style changes

--- client/src/components/gamestart/gamestart.js
+++ client/src/components/gamestart/gamestart.js
@@ -9,25 +9,24 @@ const Gamestart = () => {
             <hr/>
             <hr/>
             <div className='row'>        
-                <div className='col-xs-6'>
+                <div className='col-xs-12'>
                     {/*Player 1 Form*/}
                     <form id='player1' className="navbar-form navbar-left" role="search">
                         <div className="form-group">
                             <label htmlFor='player1'>Adress Player 1</label>
                             <input type="address" className="form-control" placeholder="Your Location"/>
                         </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                 </div>
-
-                <div className='col-xs-6'>
+            </div>
+            <div className='row'>
+                <div className='col-xs-12'>
                     {/*Player 2 Form*/}
                     <form id='player2' className="navbar-form navbar-left" role="search">
                         <div className="form-group">
                             <label htmlFor='player2'>Adress Player 2</label>
                             <input type="address" className="form-control" placeholder="Your Location"/>
                         </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
                 </div>
             </div>
@@ -49,7 +48,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/baseball.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Basketball</label>
+                            <label htmlFor='checkbox'>Basketball</label>
                             <input type='checkbox' id='basketball'/>
                             <img className='img img-thumbnail' src={require='./../images/basketball.png'}/>
                         </li>
@@ -59,7 +58,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/cryingemoji.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Disappointed Emoji</label>
+                            <label htmlFor='checkbox'>Disappointed Emoji</label>
                             <input type='checkbox' id='disappointedemoji'/>
                             <img className='img img-thumbnail' src={require='./../images/disappointedemoji.png'}/>
                         </li>
@@ -74,7 +73,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/fireball.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Heart</label>
+                            <label htmlFor='checkbox'>Heart</label>
                             <input type='checkbox' id='heart'/>
                             <img className='img img-thumbnail' src={require='./../images/heart.png'}/>
                         </li>
@@ -84,7 +83,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/huggingfaceemoji.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Monocle</label>
+                            <label htmlFor='checkbox'>Monocle</label>
                             <input type='checkbox' id='monocle'/>
                             <img className='img img-thumbnail' src={require='./../images/monocleemoji.png'}/>
                         </li>
@@ -94,7 +93,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/nerdemoji.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Poo Emoji</label>
+                            <label htmlFor='checkbox'>Poo Emoji</label>
                             <input type='checkbox' id='pooemoji'/>
                             <img className='img img-thumbnail' src={require='./../images/pooemoji.png'}/>
                         </li>
@@ -104,7 +103,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/surprisedemoji.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Thinking Emoji</label>
+                            <label htmlFor='checkbox'>Thinking Emoji</label>
                             <input type='checkbox' id='thinkingemoji'/>
                             <img className='img img-thumbnail' src={require='./../images/thinkingfaceemoji.png'}/>
                         </li>
@@ -114,7 +113,7 @@ const Gamestart = () => {
                             <img className='img img-thumbnail' src={require='./../images/vomitemoji.png'}/>
                         </li>
                         <li>
-                            <label for='checkbox'>Wacky Emoji</label>
+                            <label htmlFor='checkbox'>Wacky Emoji</label>
                             <input type='checkbox' id='wackyemoji'/>
                             <img className='img img-thumbnail' src={require='./../images/wackyemoji.png'}/>
                         </li>
@@ -130,7 +129,7 @@ const Gamestart = () => {
             <div className='row'>
                 {/*Start Game Button*/}
                 <button className='btn'>
-                    <h2 className='jumbotron rainbow monotonFont'>Start Game!</h2>
+                    <h2 className='jumbotron rainbow monotonFont'><Link to='/main1'>Start Game!</Link></h2>
                 </button>
             </div>
             <hr/>
