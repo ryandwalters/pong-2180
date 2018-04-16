e9a3d4a6b32f110afb582fdabb19b45307e2e84b
David Scherer-ODell <dscherer21@gmail.com>
2018-04-16 17:36:03 -0500

    setting up server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const bodyParser = require("body-parser");
 //const mongoose = require("mongoose");
 //const articleController = require("./controllers/articleController");
 const app = express();
-//require('dotenv').config();
-//const PORT = process.env.PORT || 3001;
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-//app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 // Add routes, both API and view
 
 //app.use(articleController);
@@ -41,15 +41,15 @@ db.once("open", function () {
 
 */
 app.get('/', function(req, res) {
-
+  res.json(JSON.parse(response.body));
 })
 
 app.get('/main1', function(req, res) {
-  
+  res.json(JSON.parse(response.body));
 })
 
 app.get('/login', function(req, res) {
-  
+  res.json(JSON.parse(response.body));
 })
 
 //if running on a deployed site use the build folder
